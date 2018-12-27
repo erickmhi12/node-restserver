422f04e8c9acb70210369812df4fc05ca772b618
Erick <ErickH@translead.com>
2018-12-27 11:12:36 -0800

    Variable de Entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://erickh:123456a@ds039135.mlab.com:39135/erickcafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
